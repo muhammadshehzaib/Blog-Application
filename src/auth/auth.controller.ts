4d50a874edb490883d3f26c73b8e10f258c433ff
m-Shehzaib <98905825+muhammadshehzaib@users.noreply.github.com>
2024-01-26 12:20:56 +0500

    Blog crud created and all the edge cases are handled

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -12,14 +12,9 @@ export class AuthController {
   @Post('login')
   async login(@Request() req): Promise<any> {
     console.log('This is req', req);
-    const user = req.user;
-    console.log('This is user', user);
-
+    const user = req.user.id;
     const token = await this.authService.login(user);
-
-    console.log('This is token', token);
-
-    return { token };
+    return { token, user };
   }
 
   @Post('register')
