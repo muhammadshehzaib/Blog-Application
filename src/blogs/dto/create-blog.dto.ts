640fe71bb6e4050e8c967205889d63d8d9c0c640
m-Shehzaib <98905825+muhammadshehzaib@users.noreply.github.com>
2024-01-27 18:31:38 +0500

    Enable users to add reactions to posts, ensuring individual user reactions are appropriately updated

--- src/blogs/dto/create-blog.dto.ts
+++ src/blogs/dto/create-blog.dto.ts
@@ -2,6 +2,7 @@ import { Auth } from 'src/auth/schemas/auth.schema';
 import { BlogsCategories } from '../../category/schemas/category.schema';
 import { Status } from '../schemas/blogs.schema';
 import { Comments } from 'src/comments/schemas/comments.schema';
+import { Reaction } from 'src/reactions/schemas/reaction.schema';
 
 export class CreateBlogDto {
   readonly title: string;
@@ -11,4 +12,5 @@ export class CreateBlogDto {
   readonly status: Status;
   readonly userId: Auth;
   readonly comments: Array<Comments>;
+  readonly reactions: Array<Reaction>;
 }
