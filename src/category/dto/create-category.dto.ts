cc3ca7c6ea7cc26a50c589a1f4178d37d583ceea
m-Shehzaib <98905825+muhammadshehzaib@users.noreply.github.com>
2024-02-20 19:25:32 +0500

    All the functionalities added to the backend according to the requirements

--- src/category/dto/create-category.dto.ts
+++ src/category/dto/create-category.dto.ts
@@ -1,6 +1,6 @@
 import { BlogsCategories } from '../schemas/category.schema';
 
 export class CreateCategoryDto {
-  readonly category: BlogsCategories;
+  readonly category: string;
   readonly createdAt: Date;
 }
