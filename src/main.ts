c924be22ea25fc8202e6ea648c34e934a0dcf71b
m-Shehzaib <98905825+muhammadshehzaib@users.noreply.github.com>
2024-02-27 14:33:59 +0500

    Correct the production links and local links

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,32 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: `${process.env.LOCALHOST || process.env.DEPLOYMENTLINK}`,
+    origin: [process.env.LOCALHOST, process.env.DEPLOYMENTLINK],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
+  if (process.env.NODE_ENV === 'production') {
+    // Production CORS configuration
+    app.enableCors({
+      origin: [process.env.DEPLOYMENTLINK],
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      credentials: true,
+      allowedHeaders:
+        'Origin,X-Requested-With,Content-Type,Accept,Authorization',
+      exposedHeaders: 'Location',
+    });
+  } else {
+    // Development CORS configuration
+    app.enableCors({
+      origin: [process.env.LOCALHOST],
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      credentials: true,
+      allowedHeaders:
+        'Origin,X-Requested-With,Content-Type,Accept,Authorization',
+      exposedHeaders: 'Location',
+    });
+  }
+
   await app.listen(3002);
 }
 bootstrap();
